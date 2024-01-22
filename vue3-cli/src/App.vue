743596bfa13a511b3d249359cbeb58f90ed0f0b7
Saehan <wjdtpgks388@gmail.com>
2024-01-23 01:40:08 +0900

    [ADD]

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AppHeader :appTitle="msg" v-on:change="changeMessage"></AppHeader>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+      msg: "앱 헤더 컴포넌트",
+    };
+  },
+  methods: {
+    changeMessage() {
+      this.msg = "변경됨";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ vue3-cli/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ appTitle }}</h1>
+    <button @click="changeTitle">눌러봐</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    appTitle: String,
+  },
+  methods: {
+    changeTitle() {
+      this.$emit("change");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
