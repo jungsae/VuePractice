166e950c0152b2e1a5abd748d3fce37b184219cd
jungsae <wjdtpgks388@gmail.com>
2024-02-05 17:54:18 +0900

    [ADD]

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <Header>
+    <MainHeader></MainHeader>
+  </Header>
   <div class="black-bg" v-if="modalStatus == true">
     <div class="white-bg">
       <h4>상세페이지</h4>
@@ -8,27 +11,24 @@
   </div>
 
   <div class="menu">
-    <a v-for="(a, i) in tags" :key="i">{{ a }}</a>
   </div>
 
   <div v-for="(data, index) in rooms" :key="index">
     <img :src="data.image" class="room-img" />
     <p>제품번호: {{ data.id }}</p>
-    <h4
-      @click="modalStatus = true"
-      style="display: inline-block; border: 1px solid seagreen"
-    >
+    <h4 @click="modalStatus = true" style="display: inline-block; border: 1px solid seagreen">
       {{ data.title }}
     </h4>
     <p>가격: {{ data.price }}</p>
     <p>설명: {{ data.content }}</p>
-    <button @click="report(index)">허위매물신고</button>
-    <p>누적신고: {{ count[index] }}</p>
+    <button @click="report(index)">장바구니: </button>
+    <p>수량: {{ count[index] }}</p>
   </div>
 </template>
 
 <script>
 import data from "./assets/data";
+import MainHeader from "./components/Main-Header.vue";
 export default {
   name: "App",
   data() {
@@ -48,7 +48,9 @@ export default {
       localStorage.setItem("count", JSON.stringify(this.count));
     },
   },
-  components: {},
+  components: {
+    MainHeader
+  },
 };
 </script>
 
@@ -56,9 +58,11 @@ export default {
 body {
   margin: 0;
 }
+
 div {
   box-sizing: border-box;
 }
+
 .black-bg {
   width: 100%;
   height: 100%;
@@ -66,17 +70,20 @@ div {
   position: fixed;
   padding: 20px;
 }
+
 .white-bg {
   width: 100%;
   background: white;
   border-radius: 8px;
   padding: 20px;
 }
+
 .room-img {
   width: 100%;
   margin-top: 40px;
   border-radius: 40px;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -90,6 +97,7 @@ div {
   padding: 15px;
   border-radius: 5px;
 }
+
 .menu a {
   color: white;
   padding: 10px;
